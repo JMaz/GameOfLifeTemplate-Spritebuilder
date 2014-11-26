55f71a20f0b45c63b9c3141b42c2acad97782996
JMaz <janmasztal@mail.adelphi.edu>
2014-11-26 18:22:01 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Creature.h
+++ GameOfLife.spritebuilder/Creature.h
@@ -17,6 +17,6 @@
 @property (nonatomic, assign) NSInteger livingNeighbors;
 
 - (id)initCreature;
-- (void)setIsAlive:(BOOL)newState;
+
 
 @end
--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -144,10 +144,10 @@ static const int GRID_COLUMNS = 10;
         for (int j = 0; j < [_gridArray[i] count]; j++){
             Creature *currentCreature = _gridArray[i][j];
             if(currentCreature.livingNeighbors == 3){
-                currentCreature.setIsAlive:true;
+                [currentCreature setIsAlive:true];
             }
             else if(currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 1 ){
-                currentCreature.setIsAlive: false;
+                [currentCreature setIsAlive:false];
             }
             }
     }
